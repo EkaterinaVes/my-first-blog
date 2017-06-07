f4b7f266aefbabc1b8a55966fbc5cd371aa82b76
Kate <ka-ve@mail.ru>
2017-06-07 15:46:05 +0300

    Changed the template for the site

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8jj!1+qb2!!=kl&w5n_no1*rfchm!3-a@z#3x1ah@5372km__e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vesnck.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
